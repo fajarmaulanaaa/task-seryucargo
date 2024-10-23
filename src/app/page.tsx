870fa18188902e849055ab730ee843a623f05592
fajarmaulanaaa <maulana19739@gmail.com>
2024-10-23 10:01:20 +0700

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 import React from 'react'
 const Homepage = dynamic(
-  () => import('@/component/pages/homePage'),
+  () => import('../component/pages/homePage'),
   { ssr: false }
 )
 
