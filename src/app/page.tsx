f13a9be7356096b06ecd14031e6c97c15645d567
fajarmaulanaaa <maulana19739@gmail.com>
2024-10-23 10:07:01 +0700

    fix deploy-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Homepage from '@/component/pages/homePage';
 import dynamic from 'next/dynamic';
 import React from 'react'
-// const Homepage = dynamic(
-//   () => import('../component/pages/homePage'),
-//   { ssr: false }
-// )
+const Homepage = dynamic(
+  () => import('../component/pages/homePage'),
+  { ssr: false }
+)
 
 export default function Home() {
   return (
