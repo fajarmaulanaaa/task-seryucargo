633867322243575655b5d5edfc1076641e922139
fajarmaulanaaa <maulana19739@gmail.com>
2024-10-23 10:51:48 +0700

    tes build-3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+import HomePage from '@/component/pages/homePage';
 import dynamic from 'next/dynamic';
 import React from 'react'
-const Homepage = dynamic(
-  () => import('../component/pages/homePage'),
-  { ssr: false }
-)
+// const Homepage = dynamic(
+//   () => import('../component/pages/homePage'),
+//   { ssr: false }
+// )
 
 export default function Home() {
   return (
-    <Homepage />
+    <HomePage />
   );
 }
