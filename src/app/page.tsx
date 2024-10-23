71fbe249beee81e7418ab179ec6cd69f74436f84
fajarmaulanaaa <maulana19739@gmail.com>
2024-10-23 10:27:24 +0700

    tes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-import dynamic from 'next/dynamic';
+
+import HomePage from '@/component/pages/homePage';
 import React from 'react'
-const Homepage = dynamic(
-  () => import('../component/pages/homePage'),
-  { ssr: false }
-)
+// const Homepage = dynamic(
+//   () => import('../component/pages/homePage'),
+//   { ssr: false }
+// )
 
 const page = () => {
   return (
     <React.Fragment>
-      <Homepage />
+      <HomePage />
     </React.Fragment>
   );
 }
