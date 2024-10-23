a0f5c4e4cdb97456605ca39eb4d5e30e1d75b2f4
fajarmaulanaaa <maulana19739@gmail.com>
2024-10-23 10:41:21 +0700

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,14 @@
-'use client'
-import NowPlaying from '@/component/organisms/NowPlaying';
-import TopRatedMovie from '@/component/organisms/TopRatedMovie';
-import ContainerPages from '@/component/templates/ContainerPages';
-import authStore from '@/context/auth/authStore';
-import favoriteStore from '@/context/favorite/favoriteStore';
-import watchlistStore from '@/context/watchlist/watchlistStore';
-import { useSearchParams } from 'next/navigation';
-import React, { useEffect } from 'react'
+import dynamic from 'next/dynamic';
+import React from 'react'
+const Homepage = dynamic(
+  () => import('../component/pages/homePage'),
+  { ssr: false }
+)
 
-const page = () => {
-  const searchParams = useSearchParams()
-  const request_token = searchParams.get('request_token');
-  const approved = searchParams.get('approved');
-  const { handleCreateSession } = authStore();
-  const { getDataWatchlistMovie } = watchlistStore();
-  const { getDataFavoriteMovie } = favoriteStore();
-  const session = localStorage.getItem('session_id')
-  useEffect(() => {
-    if (!session && request_token && approved === 'true') {
-      handleCreateSession(request_token)
-    }
-    if (session) {
-      getDataFavoriteMovie(atob(session))
-      getDataWatchlistMovie(atob(session))
-    }
-  }, [])
+export default function Home() {
   return (
     <React.Fragment>
-      <ContainerPages>
-
-        {/* now playing  */}
-        <NowPlaying />
-
-        {/* top Rate Movie  */}
-        <TopRatedMovie />
-
-      </ContainerPages>
+      <Homepage />
     </React.Fragment>
   );
 }
-
-export default page
